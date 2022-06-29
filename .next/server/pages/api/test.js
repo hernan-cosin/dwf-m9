2b8f57ec5a3d6c72b3d7e0cdaf2374cae4747412
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-06-29 08:27:41 -0300

    next example

---
+++ .next/server/pages/api/test.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/test";
+exports.ids = ["pages/api/test"];
+exports.modules = {
+
+/***/ "(api)/./pages/api/test.js":
+/*!***************************!*\
+  !*** ./pages/api/test.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ handler)\n/* harmony export */ });\nfunction handler(request, response) {\n    response.status(200).json({\n        body: request.body,\n        query: request.query,\n        cookies: request.cookies,\n        test: true\n    });\n};\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwaSkvLi9wYWdlcy9hcGkvdGVzdC5qcy5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQWUsU0FBU0EsT0FBTyxDQUFDQyxPQUFPLEVBQUVDLFFBQVEsRUFBRTtJQUNqREEsUUFBUSxDQUFDQyxNQUFNLENBQUMsR0FBRyxDQUFDLENBQUNDLElBQUksQ0FBQztRQUN4QkMsSUFBSSxFQUFFSixPQUFPLENBQUNJLElBQUk7UUFDbEJDLEtBQUssRUFBRUwsT0FBTyxDQUFDSyxLQUFLO1FBQ3BCQyxPQUFPLEVBQUVOLE9BQU8sQ0FBQ00sT0FBTztRQUN4QkMsSUFBSSxFQUFFLElBQUk7S0FDWCxDQUFDLENBQUM7Q0FDSiIsInNvdXJjZXMiOlsid2VicGFjazovL2R3Zi1tOS8uL3BhZ2VzL2FwaS90ZXN0LmpzPzZlN2EiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gaGFuZGxlcihyZXF1ZXN0LCByZXNwb25zZSkge1xyXG4gIHJlc3BvbnNlLnN0YXR1cygyMDApLmpzb24oe1xyXG4gICAgYm9keTogcmVxdWVzdC5ib2R5LFxyXG4gICAgcXVlcnk6IHJlcXVlc3QucXVlcnksXHJcbiAgICBjb29raWVzOiByZXF1ZXN0LmNvb2tpZXMsXHJcbiAgICB0ZXN0OiB0cnVlLFxyXG4gIH0pO1xyXG59XHJcbiJdLCJuYW1lcyI6WyJoYW5kbGVyIiwicmVxdWVzdCIsInJlc3BvbnNlIiwic3RhdHVzIiwianNvbiIsImJvZHkiLCJxdWVyeSIsImNvb2tpZXMiLCJ0ZXN0Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(api)/./pages/api/test.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-api-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("(api)/./pages/api/test.js"));
+module.exports = __webpack_exports__;
+
+})();
